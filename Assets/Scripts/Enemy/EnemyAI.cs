9c7df87d520621916ebfb883c1a7c96d019f0ab9
marwa <marwa@LAPTOP-MB>
2025-12-02 23:07:52 +0100

    Enemy added

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,235 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAI : MonoBehaviour
+{
+    // Statussen: 
+    public enum State { Patrolling, Chasing, Attacking }
+    public State currentState;
+
+    [Header("Onderdelen")]
+    public NavMeshAgent agent;
+    public Animator enemyAnimator;
+    public FPController playerScript;
+    public Transform playerTarget; 
+    public Transform enemyEyes;    
+    public LayerMask obstacleMask; // Wat blokkeert 
+
+    [Header("Gedrag Instellingen")]
+    public float patrolSpeed = 1.5f;
+    public float chaseSpeed = 5f;
+    public float patrolRadius = 20f; 
+
+    [Header("Zicht Instellingen (Field of View)")]
+    public float viewDistance = 15f; 
+    [Range(0, 360)] public float viewAngle = 110f;
+
+    [SerializeField] private float currentNoiseRadius;
+
+    [Header("Gehoor ")] 
+    public float hearRangeIdle = 3f;   
+    public float hearRangeWalk = 10f;  
+    public float hearRangeSprint = 25f;
+
+    [Header("Zoek Timer")] 
+    public float searchDuration = 5f; 
+    private float searchTimer;        
+
+    [Header("Aanval Instellingen")]
+    public float attackDistance = 1.2f; 
+    public GameObject jumpscareCam;     // De camera voor zijn gezicht
+    public GameObject mainPlayerCam;    // Je eigen camera
+    public AudioSource attackScream;    
+
+    private Vector3 randomDestination;
+    private bool isJumpscaring = false;
+
+    void Start()
+    {
+        currentState = State.Patrolling;
+        FindNewPatrolPoint();
+
+        if (playerScript == null && playerTarget != null)
+            playerScript = playerTarget.GetComponent<FPController>();
+    }
+
+    void Update()
+    {
+        if (isJumpscaring) return;
+        if (enemyAnimator != null)
+        {
+            enemyAnimator.SetFloat("Speed", agent.velocity.magnitude);
+        }
+        bool seesPlayer = CanSeePlayer();
+        bool hearsPlayer = CanHearPlayer();
+
+        if (seesPlayer || hearsPlayer)
+        {
+            if (hearsPlayer && !seesPlayer)
+            {
+                Debug.Log("Ik hoor je!");
+            }
+
+            currentState = State.Chasing;
+            searchTimer = searchDuration;
+        }
+        else if (currentState == State.Chasing)
+        {  
+            searchTimer -= Time.deltaTime;
+            if (searchTimer <= 0)
+            {
+                currentState = State.Patrolling;
+                FindNewPatrolPoint();
+            }
+            else
+            {
+                ChaseBehavior();
+            }
+        }
+
+        switch (currentState)
+        {
+            case State.Patrolling:
+                PatrolBehavior();
+                break;
+            case State.Chasing:
+                ChaseBehavior();
+                break;
+        }
+    }
+    void PatrolBehavior()
+    {
+        agent.speed = patrolSpeed;
+        agent.SetDestination(playerTarget.position);
+        if (agent.remainingDistance < 0.5f && !agent.pathPending)
+        {
+            FindNewPatrolPoint();
+        }
+    }
+    bool CanHearPlayer()
+    {
+        if (playerTarget == null || playerScript == null) return false;
+
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTarget.position);
+
+        
+        currentNoiseRadius = hearRangeIdle; 
+
+        if (playerScript.CurrentSpeed > 0.1f)
+        {
+            if (playerScript.Sprinting)
+            {
+                currentNoiseRadius = hearRangeSprint; 
+            }
+            else
+            {
+                currentNoiseRadius = hearRangeWalk; 
+            }
+        }
+        if (distanceToPlayer < currentNoiseRadius)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    void ChaseBehavior()
+    {
+        agent.speed = chaseSpeed;
+        agent.SetDestination(playerTarget.position); 
+
+        // Check of we dichtbij genoeg zijn voor de kill
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTarget.position);
+        if (distanceToPlayer <= attackDistance)
+        {
+            StartJumpscare();
+        }
+    }
+    // Kiest een willekeurig punt op de NavMesh om heen te lopen
+    void FindNewPatrolPoint()
+    {
+        Vector3 randomDir = Random.insideUnitSphere * patrolRadius;
+        randomDir += transform.position;
+        NavMeshHit hit;
+        // Check of het punt op de loopbare vloer ligt
+        if (NavMesh.SamplePosition(randomDir, out hit, patrolRadius, 1))
+        {
+            randomDestination = hit.position;
+            agent.SetDestination(randomDestination);
+        }
+    }
+    bool CanSeePlayer()
+    {
+        if (playerTarget == null) return false;
+
+        Vector3 dirToPlayer = (playerTarget.position - enemyEyes.position).normalized;
+        float distanceToPlayer = Vector3.Distance(enemyEyes.position, playerTarget.position);
+
+        // Afstand Check:
+        if (distanceToPlayer < viewDistance)
+        {
+            // Hoek Check
+            if (Vector3.Angle(enemyEyes.forward, dirToPlayer) < viewAngle / 2)
+            {
+                // Muren Check (Raycast)
+                // We schieten een straal van zijn oog naar het midden van de speler
+                if (!Physics.Linecast(enemyEyes.position, playerTarget.position, obstacleMask))
+                {
+                    return true; // IK ZIE JE!
+                }
+            }
+        }
+        return false;
+    }
+    void StartJumpscare()
+    {
+        isJumpscaring = true;
+        currentState = State.Attacking;
+        agent.isStopped = true; // Stop met rennen
+
+        if (enemyAnimator != null)
+        {
+            enemyAnimator.SetFloat("Speed", 0); 
+            enemyAnimator.SetTrigger("Attack"); 
+        }
+        playerScript.enabled = false;
+        // Zet de besturing uit
+        
+        // Wissel van camera
+        if (mainPlayerCam != null) mainPlayerCam.SetActive(false);
+        if (jumpscareCam != null) jumpscareCam.SetActive(true);
+        //Speel geluid
+        if (attackScream != null) attackScream.Play();
+
+        // Zorg dat de enemy je recht aankijkt voor de death cam
+        // verplaatsen recht voor de originele speler positie
+        Vector3 killPos = playerTarget.position + (playerTarget.forward * 0.8f);
+        killPos.y = transform.position.y; // Hou dezelfde hoogte
+        transform.position = killPos;
+        transform.LookAt(playerTarget);
+
+        Debug.Log("GAME OVER");
+    }
+
+    //hulp viir visueel 
+    void OnDrawGizmos()
+    {
+        // Teken Zicht
+        if (enemyEyes != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(enemyEyes.position, viewDistance);
+        }
+
+        // Teken Gehoor (Let op: dit tekenen we rondom de SPELER, niet de enemy)
+        if (playerTarget != null && Application.isPlaying)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(playerTarget.position, currentNoiseRadius);
+        }
+    }
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal) angleInDegrees += enemyEyes.eulerAngles.y;
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
